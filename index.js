5ee8394c88245f71249acc2fc56c58bd1368051d
Ravi Kumar <ravi14577@gmail.com>
2023-04-07 01:15:55 +0530

    complete ejs

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 import express from "express";
-import path from "path"
+import path from "path";
+
 const app = express();
 
+const users= [];
+//middlewere
 app.use(express.static(path.join(path.resolve(), "public")));
 
+
+app.use(express.urlencoded({extended: true}))
 
 //Settingf up view engine
 app.set("view engine","ejs");
 
-app.get("/",(req,res)=>{
-
-    // res.render("index",{ first: "Ravi"});
-    res.sendFile("index")
+app.post("/",(req,res)=>{
+   
+   users.push({username: req.body.name, email: req.body.email});
+    res.b ("success")
+   
 })
 
 app.listen(5000,() =>{
