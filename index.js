58c10df36f3e877e25a97de0a2e25c49495f6268
theravikr21 <ravi.kumar@preplaced.in>
2024-06-25 22:01:03 +0530

    leaning branch publish

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express=require('express')
+const express = require("express");
 
-const app=express()
-const port=4000
-app.get('/',(req,res)=>{
-    res.send('hello world')
-})
+const app = express();
+const port = 4000;
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
-app.listen(port,()=>{
-    console.log(`here we listen the reaponse in ${port}`)
-})
+app.listen(port, () => {
+  console.log(`here we listen the reaponse in ${port}`);
+  console.log("learning");
+});
